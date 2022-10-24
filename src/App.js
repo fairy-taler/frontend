3c18887fb22d728548c8ace0127e407628941a0c
jihi32123 <jihi32123@gmail.com>
2022-10-24 16:25:44 +0900

    #7 Add 공지사항 파일 추가

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./main/pages/Main";
 import Error from "./error/page/Error";
 import Layout from "./layouts/Layout";
+import Notice from "./community/pages/Notice";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Main />} />
+          <Route path="/notice" element={<Error />}/>
           <Route path="/*" element={<Error />} exact />
         </Route>
       </Routes>
---
+++ src/community/pages/Notice.js
@@ -0,0 +1,11 @@
+import style from "../static/css/Notice.module.css";
+
+function Notice(){
+    return (
+        <div>
+            <h1>공지사항</h1>
+        </div>
+    )
+}
+
+export default Notice;
