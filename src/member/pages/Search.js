a20e81c354f92b61e69aceda283f7ecbeb87127f
LeeYuri <56033943+lyr426@users.noreply.github.com>
2022-10-25 20:21:22 +0900

    #4 Add 회원 정보 조회 폼 추가

--- src/member/pages/Search.js
+++ src/member/pages/Search.js
@@ -9,6 +9,7 @@ import { useEffect, useState } from "react";
 function Join(){
 
     const dispatch = useDispatch();
+    const member = ""
     const header = useSelector(state => state.headerReducer);
     // const userInfo = useSelector(state => state.idSearchReducer); 
 
@@ -17,14 +18,27 @@ function Join(){
         dispatch({ type: ON_BLACK});
     },[])
 
+    const onChangeHandler = () => {
+
+    }
+
     // console.log(userInfo)
     return (
         <div className={style.searchDiv}>
             <div className={style.selectOption}>
                 <button ><img src={require("../static/images/active-id-btn.png")}/> </button>
                 <button ><img src={require("../static/images/pwd-btn.png")}/> </button>
-                
             </div>
+            <div className={style.phrase}> 아이디 찾기를 위한 본인확인을 진행해 주세요 </div>
+            <img className={style.commonImg} src={require("../static/images/mail.png")}/>
+            <div className={style.inputBox}>
+                <input type="text" name="memberName" id="memberName" value={member.memberName} onChange={ onChangeHandler } placeholder="이름" required />
+            </div>
+            <div className={style.inputBox}>
+                <input type="text" name="phone" id="phone" value={member.phone} onChange={ onChangeHandler } placeholder="이메일" required />
+                <br />
+            </div>
+            <button className={style.submitBtn}><img src={require("../static/images/id-search-btn.png")} /> </button>
         </div>
     )
 }
