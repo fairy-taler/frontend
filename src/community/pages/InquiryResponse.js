e41340759c05c9259ae548dc2ff07039ed6c0138
LeeYuri <56033943+lyr426@users.noreply.github.com>
2022-10-27 14:03:56 +0900

    #39 Refactor 답변 완료 된 문의 경우 답변 내용이 보이도록 수정

--- src/community/pages/InquiryResponse.js
+++ src/community/pages/InquiryResponse.js
@@ -11,14 +11,21 @@ function InquiryResponse(){
                     "title" :"구매한 동화책이 보이지 않아요.", 
                     "content" :"안녕하세요. " 
                                 + " \n 구매한 동화책이 보이지 않아요. 어디서 확인할 수 있나요?",
+                    "status" : "Y",
                     "date" :"2022-10-23"
                     }
     const nextResult = {"tag" :"문의",
                         "title" :"게임 오류 목록 확인", 
                                 "content" :"안녕하세요. " 
                                             + " \n ID 보호 모드를 해제하고 싶어요.",
-                                "date" :"2022-10-13"
+                        "status" : "Y",
+                        "date" :"2022-10-13"
                         }
+
+    const response = { "content" : "안녕하세요, 운영진 입니다." 
+                                    + " \n 구매한 동화책은 로그인 하신 뒤 마이페이지에서 확인하실 수 있습니다. "
+                                    + " \n 항상 책장 속 고양이를 사랑해주셔서 감사합니다. "
+                     }
                      
    
     // 헤더 설정 변경
@@ -62,9 +69,16 @@ function InquiryResponse(){
             </div>
             <img className={style.lineImg} src={require("../static/images/line.png")} />
             {/* 내용 입력란 */}
+            { result.status == 'Y' ? 
+            <div className={style.contentContentBox}>
+                {response.content}
+            </div>
+            :    
             <div className={style.contentInputBox}> 
                 <textarea className={style.contentInput} placeholder="내용을 입력해주세요."/>
             </div>
+            }
+
             {/* 등록하기 버튼 */}
             <img className={style.lineImg} src={require("../static/images/line.png")} />
             <div className={style.insertButtonBox}>
