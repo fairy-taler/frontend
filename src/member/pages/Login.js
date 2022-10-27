b61c4adace173e739cc8be620e3732693075c237
jihi32123 <jihi32123@gmail.com>
2022-10-27 11:17:23 +0900

    #34 Add navBar에서 로그인 정보 확인 기능 추가

--- src/member/pages/Login.js
+++ src/member/pages/Login.js
@@ -13,7 +13,6 @@ function Login(){
     const dispatch = useDispatch();
     const member = ""; 
     
-    
     useEffect(()=>{
         dispatch({ type: ON_CLICK, payload : false});
         dispatch({ type: ON_BLACK});
@@ -21,19 +20,17 @@ function Login(){
 
     const onChangeHandler = () => {
     }
+    
 
+    
+    // 로그인 버튼 클릭시 로그인 아이디가 'admin'일 때 로그인리덕스의 role을 admin으로 변경 ,아닌 경우 role을 teacher로 변경
     const navigate = useNavigate();
-    // 로그인 버튼 클릭시 이벤트
     const onClickLoginbutton = () =>{
-        // 로그인 버튼 클릭시 로그인 아이디가 'admin'일 때 로그인리덕스의 role을 admin으로 변경 ,아닌 경우 role을 teacher로 변경
         const id = document.getElementById("memberId");
         id.value == "admin"? dispatch({ type: LOGIN_ADMIN}) : 
                              dispatch({ type: LOGIN_TEACHER});
         navigate(`/`);
-
     }
-
-
 
     return (
         <div className={style.loginDiv}>
