6ba25a9701ccfea80212e35f8cdbd099772ae530
LeeYuri <56033943+lyr426@users.noreply.github.com>
2022-10-27 13:15:05 +0900

    #38 Add 문의 전체 리스트 조회 페이지 생성

--- src/main/components/common/Navbar.js
+++ src/main/components/common/Navbar.js
@@ -39,7 +39,7 @@ function Navbar() {
     } else if( memberRole == "admin"){
       return (
         <>
-          <Link to = "/"><div className={style.manageNav}>1:1 문의 관리</div></Link>
+          <Link to = "/inquirylist"><div className={style.manageNav}>1:1 문의 관리</div></Link>
           <Link to = "/"><div className={style.manageNav}>공지사항 관리</div></Link>
           <Link to = "/"><div className={style.manageNav}>회원 관리 </div></Link>
           <Link to = "/"><div className={style.manageNav}>동화 관리</div></Link>
