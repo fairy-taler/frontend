20f74947c6635ad7789eb1dfbc98149b2a90f8d8
jihi32123 <jihi32123@gmail.com>
2022-10-28 10:49:47 +0900

    #52 Add 게시판 상세 페이지 추가

--- src/community/pages/ForumDetail.js
+++ src/community/pages/ForumDetail.js
@@ -1,25 +1,26 @@
-import style from "../static/css/NoticeInfo.module.css";
+import style from "../static/css/ForumDetail.module.css";
 import { useSelector, useDispatch } from 'react-redux';
 import {
     ON_BLACK, ON_WHITE, ON_CLICK
 } from '../../modules/mainModules/headerModule';
 import { useEffect } from "react";
 
-function FAQInfo(){
+function ForumDetail(){
     //공지사항 정보 불러오기
-    const result = {"tag" :"문의",
-                    "title" :"게임에 대해 궁금한 점이 있습니다.", 
-                    "content" :"안녕하세요. " 
-                                + " \n 동화를 직접 만드려면 어디로 가야하나요?",
+    const result = {"tag" :"자유",
+                    "title" :"안녕하세요~", 
+                    "content" :"안녕하세요.오늘 처음 접속해봐요 다들 반갑습니다~",
                     "date" :"2022-10-23"
                     }
-    const nextResult = {"tag" :"문의",
-                        "title" :"ID 보호모드를 해제하고 싶어요.", 
+    const nextResult = {"tag" :"자유",
+                        "title" :"게임 제작할 때 팁", 
                                 "content" :"안녕하세요. " 
                                             + " \n ID 보호 모드를 해제하고 싶어요.",
                                 "date" :"2022-10-13"
                         }
                      
+    const comments =[ {"id" : "yuri" , "contents" : "안녕하세요~"},
+                      {"id" : "jihi" , "contents" : "반갑습니다!"} ]
    
     // 헤더 설정 변경
     const dispatch = useDispatch();
@@ -60,6 +61,24 @@ function FAQInfo(){
             <div className={style.contentContentBox}>
                 {result.content}
             </div>
+            <img className={style.lineImg} src={require("../static/images/line.png")} />
+            {/* 댓글창 */}
+            <div className={style.commentBox}>
+                <div className={style.commentTitle}>댓글 총 2개</div>
+                {/* 댓글 작성 */}
+                <div className={style.commentInputBox}>
+                        <div>fairytaler</div>
+                        <input className={style.commentInput} placeholder="댓글 추가..."/>
+                        <button>댓글</button>
+                </div>
+                {/* 댓글 목록 */}
+                <div>
+                    {comments.map((comment, index)=>(<div className={style.commentsBox}>
+                                                        <div className={style.id}>{comment.id}</div>
+                                                        <div className={style.contents}>{comment.contents}</div>
+                                                    </div>))}
+                </div>
+            </div>
             {/* 다음 FAQ */}
             <img className={style.lineImg} src={require("../static/images/line.png")} />
             <div className={style.nextContentsBox}>
@@ -73,4 +92,4 @@ function FAQInfo(){
     )
 }
 
-export default FAQInfo;
+export default ForumDetail;
