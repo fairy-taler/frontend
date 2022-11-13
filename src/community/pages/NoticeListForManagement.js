a9ea79d6ef026d39733f04ebde2fe985ec650a8f
jihi32123 <jihi32123@gmail.com>
2022-11-14 04:13:41 +0900

    #88 add 관리자 화면 공지사항 및 FAQ 조회 연동

--- src/community/pages/NoticeListForManagement.js
+++ src/community/pages/NoticeListForManagement.js
@@ -3,17 +3,49 @@ import { useSelector, useDispatch } from 'react-redux';
 import {
     ON_BLACK, ON_WHITE, ON_CLICK
 } from '../../modules/mainModules/headerModule';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
+import {callGetNoticesAPI } from "../../apis/community/NoticeAPICalls"
+import { callGetFaqAPI } from "../../apis/community/FaqAPICalls"
 
 function NoticeListForManagement(){
-    //공지사항 정보 불러오기
-    const results = [{"id":"1","tag":"공지", "title" : "10월 정기점검 안내", "date" :"2022-10-23", "state" : "public"},
-                     {"id":"1","tag":"공지", "title" : "10월 정기점검 안내", "date" :"2022-10-23", "state" : "public"},
-                     {"id":"1","tag":"공지", "title" : "10월 정기점검 안내", "date" :"2022-10-23", "state" : "public"},
-                     {"id":"1","tag":"공지", "title" : "10월 정기점검 안내", "date" :"2022-10-23", "state" : "private"}];
+    // 카테고리 설정 초기화
+    const [noticeCategory, setNoticeCategory] = useState("공지");
+    // 공지사항 정보 불어오기
+    const result = useSelector(state => state.noticeReducer);
+    const result2 = useSelector(state => state.faqReducer);
+    const notices = result?.noticeList?.content;
+    const faqs = result2?.faqList?.content;
+    let datas  = null;
+    if(noticeCategory == "공지") datas = notices;
+    else  datas  = faqs;
 
     const pages = Array(10).fill()
+    
+    // 헤더 설정 변경
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        dispatch({ type: ON_CLICK, payload : false});
+        dispatch({ type: ON_BLACK});
+        dispatch(callGetNoticesAPI({	
+            page:0, size:10}
+        ));
+    },[])
+
+    const onChangeSelectCategory = (e) =>{
+        setNoticeCategory(e.target.value);
+        if(e.target.value == "공지"){
+            dispatch(callGetNoticesAPI({	
+                page:0, size:10}
+            ));
+        }else{
+            dispatch(callGetFaqAPI({	
+                page:0, size:10}
+            ));
+        }
+    }
+
     //리스트 클릭시 해당 정보로 이동하는 이벤트 함수
     const navigate = useNavigate();
     const toNoticesInfo = (e) =>{
@@ -24,14 +56,6 @@ function NoticeListForManagement(){
           );
     }
     
-    // 헤더 설정 변경
-    const dispatch = useDispatch();
-
-    useEffect(()=>{
-        dispatch({ type: ON_CLICK, payload : false});
-        dispatch({ type: ON_BLACK});
-    },[])
-
     return (
         <div className={style.noticeBox}>
             <div className={style.betweenBox}>
@@ -40,7 +64,7 @@ function NoticeListForManagement(){
                 {/* 검색창 */}
                 <div className={style.searchBox}>
                     <NavLink to="/insertNotice"><img style={{height : "36px", width : "108px", margin : "5px"}} src={require("../static/images/insert-btn.png")}/></NavLink>
-                    <select className={style.searchSelect}><option>문의</option><option>공지</option></select>
+                    <select className={style.searchSelect} onChange={onChangeSelectCategory}><option value="공지">공지</option><option value="FAQ">FAQ</option></select>
                     <div className={style.searchInputBox}>
                         <input placeholder="검색어를 입력하세요."/>
                         <img src={require("../static/images/search-btn.png")}/>
@@ -51,12 +75,12 @@ function NoticeListForManagement(){
             {/* 공지사항 리스트 */}
             <div className={style.tableBox}>
                 <table className={style.communityTable}>
-                    {results.map((result, index)=>(
+                    {datas?.map((data, index)=>(
                             <tr onClick={toNoticesInfo} id={index}>
-                                    <td id={index} style={{width : "10%" , textAlign:"left"}}>[{result.tag}]</td>
-                                    <td id={index} style={{width : "60%" , textAlign:"left"}}>{result.title}</td>
-                                    <td id={index} style={{width : "10%" , textAlign:"left"}}>{result.state == "public"? "공개" : "비공개"}</td>
-                                    <td id={index} style={{width : "10%", textAlign:"right"}}>{result.date}</td>
+                                    <td id={noticeCategory=="공지"? data?.noticeCode : data?.faqCode} style={{width : "10%" , textAlign:"left"}}>[{noticeCategory}]</td>
+                                    <td id={noticeCategory=="공지"? data?.noticeCode : data?.faqCode} style={{width : "60%" , textAlign:"left"}}>{data?.title}</td>
+                                    <td id={noticeCategory=="공지"? data?.noticeCode : data?.faqCode} style={{width : "10%" , textAlign:"left"}}>{data?.public ? "공개" : "비공개"}</td>
+                                    <td id={noticeCategory=="공지"? data?.noticeCode : data?.faqCode} style={{width : "10%", textAlign:"right"}}>{data?.createDate?.substr(0,10)}</td>
                             </tr>
                     ))}
                 </table>
